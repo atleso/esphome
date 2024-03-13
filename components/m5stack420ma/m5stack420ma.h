b34180d499caf46901b5e073b1cdee1fc3b1e716
Atle Soma <atle@soma.nu>
2024-03-13 21:37:04 +0100

    rinthelp

--- components/m5stack420ma/m5stack420ma.h
+++ components/m5stack420ma/m5stack420ma.h
@@ -30,8 +30,8 @@ class M5Stack420MASensor : public sensor::Sensor, public PollingComponent, publi
     void update() override;
     void dump_config() override;
   
-    //uint16_t read_current(uint8_t channel);
-    //uint16_t read_adc_12bit(uint8_t channel);
+    uint16_t read_current(uint8_t channel);
+    uint16_t read_adc_12bit(uint8_t channel);
 
   protected:
     sensor::Sensor *current_sensor_{nullptr};
