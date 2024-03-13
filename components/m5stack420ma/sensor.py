4b2337e73f8e623b55a534b73878dff92d063e05
Atle Soma <atle@soma.nu>
2024-03-13 17:34:53 +0100

    sensor.py

--- components/m5stack420ma/sensor.py
+++ components/m5stack420ma/sensor.py
@@ -1,22 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
+from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY, CONF_UNIT_OF_MEASUREMENT, CONF_ICON, CONF_ACCURACY_DECIMALS, CONF_UPDATE_INTERVAL
 
 DEPENDENCIES = ['i2c']
-
-CONF_I2C_ADDR = 0x55
-CONF_ENABLE_12BIT_MODE = 'enable_12bit_mode'
-CONF_UPDATE_INTERVAL = 'update_interval'
 
 m5stack420ma_ns = cg.esphome_ns.namespace('m5stack420ma')
 M5Stack420MASensor = m5stack420ma_ns.class_('M5Stack420MASensor', cg.PollingComponent, i2c.I2CDevice)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 1).extend({
+CONFIG_SCHEMA = sensor.sensor_schema().extend({
     cv.GenerateID(): cv.declare_id(M5Stack420MASensor),
-    cv.Optional(CONF_ENABLE_12BIT_MODE, default=True): cv.boolean,
     cv.Optional(CONF_UPDATE_INTERVAL): cv.update_interval,
-}).extend(cv.polling_component_schema('5s')).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
+    cv.Optional(CONF_UNIT_OF_MEASUREMENT, default=UNIT_EMPTY): cv.string,
+    cv.Optional(CONF_ICON, default=ICON_EMPTY): cv.icon,
+    cv.Optional(CONF_ACCURACY_DECIMALS, default=1): cv.int_range(min=0, max=5),
+}).extend(i2c.i2c_device_schema(0x55))  # Assuming 0x55 is the default I2C address
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
