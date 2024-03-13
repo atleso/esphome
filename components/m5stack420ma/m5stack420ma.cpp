3e1cdd782b91c8375f180e77e4b3b92ee8df2dcc
Atle Soma <atle@soma.nu>
2024-03-13 16:26:20 +0100

    cpp update

--- components/m5stack420ma/m5stack420ma.cpp
+++ components/m5stack420ma/m5stack420ma.cpp
@@ -1,26 +1,37 @@
 #include "esphome.h"
 #include "MODULE_4_20MA.h"
 
-class M5Stack420MASensor : public PollingComponent, public Sensor {
+class M5Stack420MASensor : public PollingComponent {
  public:
-  MODULE_4_20MA m5stack420ma;
+  // Create a sensor instance as a member, not by inheritance
+  Sensor *current_sensor{nullptr};
+
+  // Constructor for initializing the sensor with a polling interval
+  M5Stack420MASensor() : PollingComponent(5000) {} // Polling every 5 seconds
+
+  void set_sda_pin(uint8_t sda_pin) {
+    this->custom_sda_pin = sda_pin;
+  }
+
+  void set_scl_pin(uint8_t scl_pin) {
+    this->custom_scl_pin = scl_pin;
+  }
+
   void setup() override {
-    // Initialization with custom SDA, SCL pins if specified in YAML
+    // Assuming the `begin` method configures the module correctly
     m5stack420ma.begin(&Wire, MODULE_4_20MA_ADDR, custom_sda_pin, custom_scl_pin);
   }
 
   void update() override {
-    // Example: Read current value from channel 0
+    // Here you would read from your sensor and publish the value
     uint16_t current_value = m5stack420ma.getCurrentValue(0);
-    publish_state(current_value);
-  }
-
-  void set_pins(uint8_t sda_pin, uint8_t scl_pin) {
-    this->custom_sda_pin = sda_pin;
-    this->custom_scl_pin = scl_pin;
+    if(current_sensor) {
+      current_sensor->publish_state(current_value);
+    }
   }
 
  private:
-  uint8_t custom_sda_pin = 1; // Default SDA pin
-  uint8_t custom_scl_pin = 2; // Default SCL pin
+  MODULE_4_20MA m5stack420ma;
+  uint8_t custom_sda_pin = 2; // Default SDA pin
+  uint8_t custom_scl_pin = 1; // Default SCL pin
 };
