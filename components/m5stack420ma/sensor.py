48fadff95828222b2449c13b7512bc18df83da34
Atle Soma <atle@soma.nu>
2024-03-13 16:12:30 +0100

    updated config schema

--- components/m5stack420ma/sensor.py
+++ components/m5stack420ma/sensor.py
@@ -9,13 +9,8 @@ CONF_ENABLE_12BIT_MODE = 'enable_12bit_mode'
 m5stack420ma_ns = cg.esphome_ns.namespace('m5stack420ma')
 M5Stack420MASensor = m5stack420ma_ns.class_('M5Stack420MASensor', cg.PollingComponent, sensor.Sensor)
 
-CONFIG_SCHEMA = sensor.sensor_schema(M5Stack420MASensor, cg.PollingComponent).extend({
+CONFIG_SCHEMA = sensor.sensor_schema().extend({
     cv.GenerateID(): cv.declare_id(M5Stack420MASensor),
     cv.Optional(CONF_ENABLE_12BIT_MODE, default=False): cv.boolean,
-}).extend(cv.polling_component_schema('60s'))
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield sensor.register_sensor(var, config)
-    cg.add(var.set_enable_12bit_mode(config[CONF_ENABLE_12BIT_MODE]))
+    cv.Optional(CONF_UPDATE_INTERVAL): cv.update_interval,
+})
