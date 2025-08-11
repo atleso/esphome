b94c08315c65cb2e8012f47b740642a31df3fbc1
Soma <atle@soma.nu>
2025-08-11 20:50:29 +0200

    bugfixing

--- components/save_vtr/sensor.py
+++ components/save_vtr/sensor.py
@@ -3,6 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID, CONF_UNIT_OF_MEASUREMENT, CONF_ICON, CONF_ACCURACY_DECIMALS
 from . import save_vtr_ns, SaveVTRClimate
+import esphome.components.sensor as sensor_core
 
 
 DEPENDENCIES = ["save_vtr"]
@@ -45,3 +46,5 @@ async def to_code(config):
         if name in config:
             sens = await sensor.new_sensor(config[name])
             cg.add(getattr(paren, f"set_{name}_sensor")(sens))
+
+sensor_core.register_sensor("save_vtr", CONFIG_SCHEMA, to_code)
