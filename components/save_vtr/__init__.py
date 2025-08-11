62576535245836abc3a16c3cf7742b0406b32f11
Soma <atle@soma.nu>
2025-08-11 20:48:56 +0200

    --

--- components/save_vtr/__init__.py
+++ components/save_vtr/__init__.py
@@ -1,14 +1,11 @@
-
 import esphome.codegen as cg
 from esphome.components import climate
-from . import sensor as sensor_platform
 
 save_vtr_ns = cg.esphome_ns.namespace("save_vtr")
 SaveVTRClimate = save_vtr_ns.class_("SaveVTRClimate", climate.Climate, cg.PollingComponent)
 
 CODEOWNERS = ["@atleso"]
 
-# Ensure both platforms are auto-loaded
 AUTO_LOAD = ["climate", "sensor"]
 
 
